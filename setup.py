afbe843b5c729634c7b3d15eb9cd27aaa7454224
Arhisan <pisaga@yandex.ru>
2016-08-01 00:23:35 +0800

    Upload Info was added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+      name = 'Facebook-Bot-Library',
+      packages = ['Facebook-Bot-Library'],
+      version = '0.1',
+      description = 'A facebook messaging SDK for Flask',
+      author = 'boostedLab',
+      author_email = 'issues@boostedlab.ru',
+      url = 'https://github.com/boosted-lab/Facebook-Bot-Library',
+      download_url = 'https://github.com/boosted-lab/Facebook-Bot-Librarytarball/0.1',
+      install_requires=['requests', 'Flask'],
+      keywords = ['Facebook', 'Messenger', 'SDK', 'Bot', 'Flask'],
+      classifiers = [],
+      )
