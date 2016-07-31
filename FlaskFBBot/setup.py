be83f6605a2aabe935bf93795023be3fc880af6e
Arhisan <pisaga@yandex.ru>
2016-07-31 18:52:55 +0800

    Commit to upload

---
+++ FlaskFBBot/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+      name = 'FBBlib',
+      packages = ['FBBlib'],
+      version = '0.1',
+      description = 'A facebook messaging SDK for Flask',
+      author = 'boostedLab',
+      author_email = 'issues@boostedlab.ru',
+      url = '', # use the URL to the github repo
+      download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+      keywords = ['Facebook', 'Messenger', 'SDK', 'Bot', 'Flask'], # arbitrary keywords
+      classifiers = [],
+      )
